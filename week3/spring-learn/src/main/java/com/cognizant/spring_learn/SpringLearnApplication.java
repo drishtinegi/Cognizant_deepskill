7d663f01c25138e7abe6c18ea324186103f1ee87
drishtinegi <drishtinegi05@gmail.com>
2026-07-08 20:43:34 +0530

    Complete Week 3 Hands-on 4

--- week3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ week3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
@@ -16,6 +18,18 @@ public class SpringLearnApplication {
         SpringApplication.run(SpringLearnApplication.class, args);
 
         LOGGER.info("Inside main");
+
+        displayCountry();
     }
 
+    private static void displayCountry() {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country =
+                context.getBean("country", Country.class);
+
+        LOGGER.debug("Country : {}", country.toString());
+    }
 }
